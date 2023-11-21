4ea92fc9aab0474ee69ef09f6770d59c98f7d832
nimi31 <sharmanimisha45@gmail.com>
2023-11-21 09:59:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+// src/App.js
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://randomuser.me/api/?page=1&results=1&seed=abc');
+        setUserData(response.data.results[0]);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {userData && (
+        <div className="card">
+          <div className="card-img">
+            <img src={userData.picture.large} alt="Profile" />
+          </div>
+          <div className="info">
+            <h2>{`${userData.name.first} ${userData.name.last}`}</h2>
+            <p>{userData.gender}</p>
+            <p>{userData.phone}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
